51657ec0f6bdeee28c83fa1df12f19da4577060b
Ammar Ahmed <ammarahmed1994@hotmail.com>
2021-06-20 19:21:22 +0200

    setting up the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import Main from '../src/components/blogs/main.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import BlogList from './components/blogs/list';
+import BlogNew from './components/blogs/new';
+import BlogUpdate from './components/blogs/update';
+import Dashboard from './components/Dashboard/dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+  return <>
+      <BrowserRouter>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/blog/new" component={BlogNew} />
+        <Route path="/blog/list" component={BlogList} />
+        <Route path="/blog/:id/details" component={BlogUpdate} />
+      </BrowserRouter>
+  </>;
 }
 
 export default App;
